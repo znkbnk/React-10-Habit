ffe39af2b327aac3357a6ba00beed8d4f0f08496
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-20 19:51:17 +0100

    Create HabitForm.js

---
+++ HabitForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const HabitForm = ({ addHabit }) => {
+  const [habit, setHabit] = useState("");
+
+  const handleHabitChange = (event) => {
+    setHabit(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    addHabit(habit);
+    setHabit("");
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type='text'
+        value={habit}
+        onChange={handleHabitChange}
+        placeholder='Enter a new habit'
+      />
+      <button type='submit'>Add Habit</button>
+    </form>
+  );
+};
+
+export default HabitForm;
